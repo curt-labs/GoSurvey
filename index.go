6e7bef9ff9b4dc138e5117f46f96aad56374882d
Alex Ninneman <alex@ninneman.org>
2014-05-01 00:28:30 -0500

    added logstash file

--- index.go
+++ index.go
@@ -6,17 +6,21 @@ import (
 	"github.com/curt-labs/GoSurvey/models/warranties"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/binding"
+	"github.com/martini-contrib/logstasher"
 	"github.com/martini-contrib/render"
+	"github.com/mipearson/rfw"
 	"html/template"
 	"log"
 	"net/http"
 )
 
 var (
-	listenAddr = flag.String("port", ":8080", "http listen address")
+	listenAddr  = flag.String("port", ":8080", "http listen address")
+	environment = flag.String("env", "", "current app environment")
 )
 
 func main() {
+	flag.Parse()
 	m := martini.Classic()
 	m.Use(martini.Static("public"))
 	m.Use(render.Renderer(render.Options{
@@ -29,6 +33,13 @@ func main() {
 		IndentJSON:      true,
 		HTMLContentType: "text/html",
 	}))
+
+	if *environment == "prod" {
+		logFile, err := rfw.Open("requests.log", 0644)
+		if err == nil {
+			m.Use(logstasher.Logger(logFile))
+		}
+	}
 
 	m.Group("/api/warranty", func(r martini.Router) {
 		r.Get("", warranty.All)
