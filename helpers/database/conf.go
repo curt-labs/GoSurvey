2da07b09c0e8c3d8b0c8cd9a213a5417a7dda9a2
Alex Ninneman <alex@ninneman.org>
2014-05-06 14:29:36 -0500

    switching encoded slash to unencoded

--- helpers/database/conf.go
+++ helpers/database/conf.go
@@ -10,8 +10,8 @@ func ConnectionString() string {
 		proto := os.Getenv("DATABASE_PROTOCOL")
 		user := os.Getenv("DATABASE_USERNAME")
 		pass := os.Getenv("DATABASE_PASSWORD")
-		return fmt.Sprintf("%s:%s@%s(%s)/CurtDev?parseTime=true&loc=America%2FChicago", user, pass, proto, addr)
+		return fmt.Sprintf("%s:%s@%s(%s)/CurtDev?parseTime=true&loc=America/Chicago", user, pass, proto, addr)
 	}
 
-	return "root:@tcp(127.0.0.1:3306)/CurtDev?parseTime=true&loc=America%2FChicago"
+	return "root:@tcp(127.0.0.1:3306)/CurtDev?parseTime=true&loc=America/Chicago"
 }
