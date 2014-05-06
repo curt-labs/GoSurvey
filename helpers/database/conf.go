aeefdc27beb67ab519a22a919f32e544dd5a9ba7
Alex Ninneman <alex@ninneman.org>
2014-05-06 14:35:52 -0500

    removed encoded location out of fmt layout

--- helpers/database/conf.go
+++ helpers/database/conf.go
@@ -10,7 +10,7 @@ func ConnectionString() string {
 		proto := os.Getenv("DATABASE_PROTOCOL")
 		user := os.Getenv("DATABASE_USERNAME")
 		pass := os.Getenv("DATABASE_PASSWORD")
-		return fmt.Sprintf("%s:%s@%s(%s)/CurtDev?parseTime=true&loc=America/Chicago", user, pass, proto, addr)
+		return fmt.Sprintf("%s:%s@%s(%s)/CurtDev?parseTime=true&loc=%s", user, pass, proto, addr, "America%2FChicago")
 	}
 
 	return "root:@tcp(127.0.0.1:3306)/CurtDev?parseTime=true&loc=America/Chicago"
