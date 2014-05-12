007003fe2406f226fb615262197f1a4c8e0ee989
Alex Ninneman <alex@ninneman.org>
2014-05-12 11:31:43 -0500

    styled the survey page a little better and added select/input logic switch

--- public/js/controllers/SurveyController.js
+++ public/js/controllers/SurveyController.js
@@ -3,6 +3,22 @@ define([],function(){
 
 	var ctlr = ['$scope', '$route', '$routeParams', 'SurveyService',function($scope, $route, $routeParams, SurveyService){
 		SurveyService.get({id: $routeParams.id}, function(survey){
+			for (var i = 0; i < survey.questions.length; i++) {
+				var answers = [];
+				for (var j = 0; j < survey.questions[i].answers.length; j++) {
+					var ans = survey.questions[i].answers[j];
+					if(ans.data_type == 'multiple'){
+						if(survey.questions[i].selects === undefined){
+							survey.questions[i].selects = [];
+						}
+						survey.questions[i].selects.push(ans);
+					}else{
+						answers.push(ans);
+					}
+				}
+				survey.questions[i].answers = answers;
+			}
+
 			$scope.survey = survey;
 		});
 	}];
