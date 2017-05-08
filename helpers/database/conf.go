efa3692c7606545469aaf5b3145f5c9cc6183bb4
davidvaini <davidvaini@gmail.com>
2017-05-08 14:03:52 -0500

    uses env variable for database name, with CurtData fallback

--- helpers/database/conf.go
+++ helpers/database/conf.go
@@ -10,8 +10,12 @@ func ConnectionString() string {
 		proto := os.Getenv("DATABASE_PROTOCOL")
 		user := os.Getenv("DATABASE_USERNAME")
 		pass := os.Getenv("DATABASE_PASSWORD")
-		return fmt.Sprintf("%s:%s@%s(%s)/CurtDev?parseTime=true&loc=%s", user, pass, proto, addr, "America%2FChicago")
+		databaseName := os.Getenv("CURT_DEV_NAME")
+		if databaseName == "" {
+			databaseName = "CurtData"
+		}
+		return fmt.Sprintf("%s:%s@%s(%s)/%s?parseTime=true&loc=%s", user, pass, proto, addr, databaseName, "America%2FChicago")
 	}
 
-	return "root:@tcp(127.0.0.1:3306)/CurtDev?parseTime=true&loc=America%2FChicago"
+	return "root:@tcp(127.0.0.1:3306)/CurtData?parseTime=true&loc=America%2FChicago"
 }
