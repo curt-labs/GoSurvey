43326bc7eea2235a435200fb8ab8beb64cdb8dd0
davidvaini <davidvaini@gmail.com>
2017-08-28 15:25:33 -0500

    fixed styling on warranty boxes, also fixed latest warranties count to 10 to show all warranties rather than just 4

--- public/js/controllers/HomeController.js
+++ public/js/controllers/HomeController.js
@@ -1,7 +1,7 @@
 define([],function(){
 	'use strict';
 	var ctlr = ['$scope','SurveyService', function($scope, SurveyService){
-		SurveyService.query({page:'1', count:'4'},function(surveys){
+		SurveyService.query({page:'1', count:'10'},function(surveys){
 			$scope.surveys = surveys.surveys;
 		});
 	}];
